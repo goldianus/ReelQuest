8e1acd45d45fa536e4d9185f9fdec47203d0237a
Goldianus Solangius Mbete <goldianus.mbete@erajaya.com>
2023-09-12 22:03:22 +0700

    Initial Commit

---
+++ ReelQuest/ReelQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelQuestApp.swift
+//  ReelQuest
+//
+//  Created by Goldianus Solangius on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ReelQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
